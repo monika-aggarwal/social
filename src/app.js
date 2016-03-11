4cae330b4eb6dea5bb2e63c9e825cde5835b4028
Monika Aggarwal <monika@PT-MAC-78.local>
2016-03-11 16:05:08 +0530

    new

---
+++ src/app.js
@@ -0,0 +1,25 @@
+
+
+ $('.mainListingCard').each(function(){
+      var config = {
+        id: this.id,
+        facebook:{
+                    method: 'feed',
+                    name: $(this).children('label').text(),
+                    link: $(this).attr('data-url'),
+                    picture: $(this).find('.listing-left').find('img').attr('src'),
+                    caption: 'This is the content of the "caption" field.',
+                    description: $(this).find('.listing-right').find('.descriptionjs').text(),
+
+                    message: ''
+                },
+        googleplus:{
+                    url:  $(this).attr('data-url')
+            },
+        twitter:{
+        			url:  $(this).attr('data-url')
+        }    
+
+       }
+      $('#' + this.id + ' .sharebutton').createshare(config);
+    });
--- src/socialshare.js
+++ src/socialshare.js
@@ -1,16 +1,30 @@
 (function($){
-	var opt;
+	 var opt;
 	var template=function(){
-      return "<a id='fb_share' >"+"<img src='./image/fb.png' style='height:30px; width:30px;'>"+"</a>"+
-      		 "<a id='g_share'><img src='https://www.gstatic.com/images/icons/gplus-32.png' alt='Share on Google+'/></a>"+
-      		 "<a id='mail_share'><img src='./image/mail-icon.png' style='width:40px; height:40px;'></a>";
+      return "<span class='socialPluginButtons'><a class='fb_share' >"+"<img src='./image/fb.png' style='height:30px; width:30px;'>"+"</a>"+
+      		   "<a class='g_share'><img src='https://www.gstatic.com/images/icons/gplus-32.png' alt='Share on Google+'/></a>"+
+      		   
+             "<a class='t_share'><img src='./image/tweet.jpeg' style='width:40px;height:40px;'></a><span>";
 	};
+  
+  
+  $.fn.createshare=function(config){
+    var temp=template();
+    this.append(temp); 
+    $('#' + config.id + ' .g_share').click(function(){
+       var url=config.googleplus.url;
+       window.location.href="https://plus.google.com/share?url={"+url+"}";
+      });
+    $('#' + config.id + ' .fb_share').click(function(){
+      FB.ui(config.facebook);
+    });
+    $('#' + config.id + ' .t_share').click(function(){
+      window.location.href="https://twitter.com/intent/tweet?text="+config.twitter.url+"";
+    });
+  };
+  
+	
 
-	$.fn.socialShare=function(options){
-		opt=options;	
-		var temp=template();
-		this.append(temp);	
-    };
     window.fbAsyncInit = function() {
           FB.init({
               appId      : '983371245066475',
@@ -26,19 +40,23 @@
           fjs.parentNode.insertBefore(js, fjs);
           }(document, 'script', 'facebook-jssdk'));
 
-      $(document).ready(function(){
-          $('#fb_share').click(function(e){
-            e.preventDefault();
-            FB.ui(opt.facebook);
-          });
-          $('#g_share').click(function(){
-          	var url=opt.googleplus.url;
-          	window.location.href="https://plus.google.com/share?url={"+url+"}";
-          });
-          $('#mail_share').click(function(){
-            window.location.href="mailto:?subject="+opt.mail.subject+"&amp;body="+opt.mail.mailbody+"";
-          });
+      // $(document).ready(function(){
+      //     $('#fb_share').click(function(e){
+      //       e.preventDefault();
+      //       FB.ui(opt.facebook);
+      //     });
+      //     $('#g_share').click(function(){
+      //     	var url=opt.googleplus.url;
+      //     	window.location.href="https://plus.google.com/share?url={"+url+"}";
+      //     });
+      //     $('#mail_share').click(function(){
+      //       window.location.href="mailto:?subject="+opt.mail.subject+"&amp;body="+opt.mail.mailbody+"";
+      //     });
           
-      });
+      // });
+      // "<a class='mail_share'><img src='./image/mail-icon.png' style='width:40px; height:40px;'></a>"+
+ 
 
 }(jQuery));
+
+
