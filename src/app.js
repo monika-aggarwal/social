8f628c1ff748d54381dc1dea3d1d7dd1bb145c00
Monika Aggarwal <monika@PT-MAC-78.local>
2016-03-16 14:41:23 +0530

    formatting changes

--- src/app.js
+++ src/app.js
@@ -4,28 +4,27 @@
       var config = {
         id: this.id,
         facebook:{
-                    method: 'feed',
-                    name: $(this).children('label').text(),
-                    link: $(this).attr('data-url'),
-                    picture: $(this).find('.listing-left').find('img').attr('src'),
-                    caption: 'This is the content of the "caption" field.',
-                    description: $(this).find('.listing-right').find('.descriptionjs').text(),
-
-                    message: ''
-                },
+            method: 'feed',
+            name: $(this).children('label').text(),
+            link: $(this).attr('data-url'),
+            picture: $(this).find('.listing-left').find('img').attr('src'),
+            caption: 'This is the content of the "caption" field.',
+            description: $(this).find('.listing-right').find('.descriptionjs').text(),
+            message: ''
+        },
         googleplus:{
-                    url:  $(this).attr('data-url')
-            },
+            url:  $(this).attr('data-url')
+        },
         twitter:{
-        			url:  $(this).attr('data-url')
+            url:  $(this).attr('data-url')
         },  
         mail:{
-                    url:  $(this).attr('data-url')         
+            url:  $(this).attr('data-url')         
         },
         whatsapp:{
-                    url:  $(this).attr('data-url')
+            url:  $(this).attr('data-url')
         }
 
-       };
-      $('#' + this.id + ' .sharebutton').createshare(config);
-    });
+    };
+      $(this).find('.sharebutton').createshare(config);
+});
--- src/socialshare.js
+++ src/socialshare.js
@@ -2,39 +2,45 @@
 	 var opt;
 	var template=function(){
       return "<span class='socialPluginButtons'><a class='fb_share' >"+"<img src='./image/fb-icon.png' style='height:30px; width:30px;'>"+"</a>"+
-      		   "<a class='g_share'><img src='https://www.gstatic.com/images/icons/gplus-32.png' alt='Share on Google+'/></a>"+
       		   "<a class='m_share' >"+"<img src='./image/mail+icon.png' style='height:30px; width:30px;'>"+"</a>"+
-             "<a class='t_share'><img src='./image/tweet.png' style='width:30px;height:30px;'></a><span>"+
-             "<a class='w_share'><img src='./image/whatsapp.png' style='width:30px;height:30px;'></a><span>";
+             "<a class='w_share  mobile_display' ><img src='./image/whatsapp.png' style='width:30px;height:30px;'></a><span>";
 	};
   
   
   $.fn.createshare=function(config){
     var temp=template();
     this.append(temp); 
-    $('#' + config.id + ' .g_share').click(function(){
-       var url=config.googleplus.url;
-       window.location.href="https://plus.google.com/share?url={"+url+"}";
-      });
-    $('#' + config.id + ' .fb_share').click(function(){
+    var subject="I want you to have a look at this property";
+    $(this).find('.g_share').click(function(){
+    var url=config.googleplus.url;
+    window.location.href="https://plus.google.com/share?url={"+url+"}";
+    });
+
+    $(this).find('.fb_share').click(function(){
       FB.ui(config.facebook);
     });
-    $('#' + config.id + ' .t_share').click(function(){
+
+    $(this).find('.t_share').click(function(){
       window.location.href="https://twitter.com/intent/tweet?text="+config.twitter.url+"";
     });
-    $('#' + config.id + ' .m_share').click(function(){
-      window.location.href="mailto:?subject=I want you to have a look at this property&amp;body="+config.mail.url+"";
+
+    $(this).find('.m_share').click(function(){
+    window.location.href="mailto:?subject="+subject+"&amp;body="+config.mail.url+"";
     });
-    $('#' + config.id + ' .w_share').click(function(){
-      if( isMobile.any() ) {
-            var url = config.whatsapp.url;
-            var text = " want you to have a look at this property";
-            var message = encodeURIComponent(text) + " - " + encodeURIComponent(url);
-            var whatsapp_url = "whatsapp://send?text=" + message;
-            window.location.href = whatsapp_url;
-        } else {
-            alert("Please share this article in mobile device");
-        }
+
+    if(isMobile.any())
+          $('.w_share').toggleClass('mobile_display');
+
+    $(this).find('.w_share').click(function(){
+    if( isMobile.any() ) {
+          var url = config.whatsapp.url;
+          var text = " want you to have a look at this property";
+          var message = encodeURIComponent(text) + " - " + encodeURIComponent(url);
+          var whatsapp_url = "whatsapp://send?text=" + message;
+          window.location.href = whatsapp_url;
+    } else{
+          alert("Please share this article in mobile device");
+      }
     });
   };
   
@@ -74,23 +80,7 @@
           fjs.parentNode.insertBefore(js, fjs);
           }(document, 'script', 'facebook-jssdk'));
 
-      // $(document).ready(function(){
-      //     $('#fb_share').click(function(e){
-      //       e.preventDefault();
-      //       FB.ui(opt.facebook);
-      //     });
-      //     $('#g_share').click(function(){
-      //     	var url=opt.googleplus.url;
-      //     	window.location.href="https://plus.google.com/share?url={"+url+"}";
-      //     });
-      //     $('#mail_share').click(function(){
-      //       window.location.href="mailto:?subject="+opt.mail.subject+"&amp;body="+opt.mail.mailbody+"";
-      //     });
-          
-      // });
-      // "<a class='mail_share'><img src='./image/mail-icon.png' style='width:40px; height:40px;'></a>"+
- 
-
+      
 }(jQuery));
 
 
